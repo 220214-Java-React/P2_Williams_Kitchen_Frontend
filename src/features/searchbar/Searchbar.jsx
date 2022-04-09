65f8c5e3bd42b6c72a8f4b7e2b44fc2934c7ca73
NBryant83 <nakisha.bryant@gmail.com>
2022-04-09 18:29:24 -0400

    refactored app.js(removed boilerplate and added Searchbar tag), added the features and searchbar folders, along with Searchbar.jsx, and .env(added api key), added .env to .gitignore

---
+++ src/features/searchbar/Searchbar.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+
+const Searchbar = () => {
+   const [searchInput, setSearchInput] = useState("");
+
+
+   return (
+
+      <>
+         <div className="body">
+            
+            <div className="main">
+
+            </div>
+
+         </div>
+         <input type="text" placeholder="Enter a food item" />
+      </>
+   )
+}
+
+export default Searchbar;

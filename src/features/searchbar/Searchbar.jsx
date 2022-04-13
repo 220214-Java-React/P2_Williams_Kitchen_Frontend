96974f374dddea910c3449b91600f2b08c4e5455
NBryant83 <nakisha.bryant@gmail.com>
2022-04-13 09:53:17 -0400

    deleted navbar and searchform files along with Requests.jsx and SearchForm.jsx.

--- src/features/searchbar/Searchbar.jsx
+++ src/features/searchbar/Searchbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import axios, { Requests } from 'axios';
+import Select from "react-select"
+
 
 
 // TODO: figure out what's going on with .env files 
@@ -8,8 +10,30 @@ import axios from 'axios';
 const SearchBar = () => {
    
    const [searchInput, setSearchInput] = useState("");
+   const [multiSearchInput, setMultiSearchInput] = useState("");
+   const [selectedOption, setSelectedOption] = useState(null);
+   
+   
+   const mealTimes = [
+      { value: "breakfast", label: " Breakfast" },
+      { value: "snack", label: " Snack" },
+      { value: "lunch", label: " Lunch" },
+      { value: "dinner", label: " Dinner" },
+      { value: "smoothie", label: " Smoothie" },
+   ]
 
+   const mealSugg = [
+      { value: "chicken", label: "Chicken" },
+      { value: "broccoli", label: "broccoli" },
+      { value: "squash", label: "squash" },
+      { value: "rice", label: "rice" },
+      { value: "seaweed", label: "seaweed" },
 
+   ]
+
+      
+   
+   // function to send
    const handleSubmit = (e) => {
       e.preventDefault();
       console.log("Form has been submitted, it's recipe time!!😋")
@@ -20,38 +44,50 @@ const SearchBar = () => {
 
    const handleInput = (e) => {
       setSearchInput(e.target.value);
-      
+
    }
 
    useEffect(() => {
-      
+
       async function getData() {
          try {
-            const res = await axios.get(`www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`);
-            console.log(res.data.results)
-            
-         } catch(err) {
+            const request = await axios.get(``);
+            console.log(request.data.results)
+
+         } catch (err) {
             console.log(err)
          }
       }
       getData();
    }, [])
-   
+
+
+
+
 
 
    return (
 
       <>
-         <div className="body">                        
+         <div className="body">
             <div className="main">
                <form onSubmit={handleSubmit}>
                   <label htmlFor="search input">What's for eating 😋 </label>
                   <input type="text" placeholder="Enter a food item" name={searchInput} value={searchInput} onChange={handleInput} autoFocus />
                   <input type="submit" value="Submit" />
+
+
+                  <Select value={selectedOption} onChange={setSelectedOption} options={mealTimes}></Select>
+
+                  <Select value={selectedOption} onChange={setSelectedOption} options={mealSugg} isMulti></Select>
                   
                </form>
+               <div className="resultTable">
+
+               </div>
             </div>
          </div>
+         
       </>
    )
 }
